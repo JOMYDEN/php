90ffb20814fcd0d5cd8fbb9a69b54056600dc8fc
JOMYDEN <6621061001@cdti.ac.th>
2025-07-01 12:04:24 +0700

    Update

---
+++ header.php
@@ -0,0 +1,6 @@
+<ul>
+  <li><a href="default.asp">HOME</a></li>
+  <li><a href="news.asp">NEWS</a></li>
+  <li><a href="contact.asp">CONTACT</a></li>
+  <li><a href="about.asp">ABOT</a></li>
+</ul>
---
+++ index.php
@@ -0,0 +1,23 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+        <?php
+        include'header.php';
+    ?>
+        <h2>ยินดีต้อนรับเข้าสู่เว็บไซต์ของ Master_Jo</h2>
+        <p>จงมอบสิ่งล้ำค่าของเจ้ามา</p>
+        <div id="aaa">
+        ไมงั้น นายกอยู่ต่อ
+        <div>
+
+        <a href="#" class="btn">เงิน</a>
+        <a href="#" class="reg">ใจ</a>
+
+    <body>
+    </html>
